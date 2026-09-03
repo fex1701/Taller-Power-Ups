360c013e87b330d2230adf1d3a5e55a0c841271e
Juanfe456 <259488678+Juanfe456@users.noreply.github.com>
2026-09-03 10:31:09 -0500

    Animaciones del Agent

--- My project/Assets/_main/Scripts/Enemy/EnemyAniamtion.cs
+++ My project/Assets/_main/Scripts/Enemy/EnemyAniamtion.cs
@@ -7,20 +7,12 @@ public class EnemyAniamtion : MonoBehaviour
     public NavMeshAgent agente;
     [SerializeField] private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-
-
-    {
-
-        agente = GetComponent<NavMeshAgent>();
-
-        bool IsRunning1 = agente.transform.position == agente.destination;
-        animator.SetBool("IsRunning1", IsRunning1);
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool corriendo = agente.velocity.magnitude > 0.1f;
+        animator.SetBool("IsRunning1", corriendo);
     }
 }
