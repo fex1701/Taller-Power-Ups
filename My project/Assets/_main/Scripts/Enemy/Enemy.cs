5d8e4d9ca6db6d581417668d72bed21ffc4b3de9
Juanfe456 <259488678+Juanfe456@users.noreply.github.com>
2026-09-10 09:57:34 -0500

    Knucles y Tails

--- My project/Assets/_main/Scripts/Enemy/Enemy.cs
+++ My project/Assets/_main/Scripts/Enemy/Enemy.cs
@@ -4,7 +4,11 @@ using UnityEngine.AI;
 public class FollowPlayer : MonoBehaviour
 {
     public NavMeshAgent agente;
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private float tiempoEntreDaños = 0.5f;
+    [SerializeField] private int _damage;
     public Transform jugador;
+    private bool puedeHacerDaño = true;
     private void Start()
     {
         agente = GetComponent<NavMeshAgent>();
@@ -14,5 +18,25 @@ public class FollowPlayer : MonoBehaviour
     private void Update()
     {
         agente.destination = jugador.position;
+
+        
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && puedeHacerDaño)
+        {
+            puedeHacerDaño = false;
+
+            gameManager.RestarVida(_damage);
+
+            Invoke(nameof(ReactivarDanio), tiempoEntreDaños);
+        }
+
+    }
+
+    private void ReactivarDanio()
+    {
+        puedeHacerDaño = true;
     }
 }
